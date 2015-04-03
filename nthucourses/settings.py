a80325a99b411667e2d7cab528d98f014f8d4135
afg <afg984@gmail.com>
2015-04-03 22:16:34 +0800

    hello world page

--- nthucourses/settings.py
+++ nthucourses/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'common',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -58,12 +59,7 @@ TEMPLATES = [
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+            'environment': 'nthucourses.jinja2.environment',
         },
     },
 ]
--- nthucourses/urls.py
+++ nthucourses/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from common.views import Index
+
 urlpatterns = patterns('',
+    url(r'^$', Index.as_view(), name='index'),
     # Examples:
     # url(r'^$', 'nthucourses.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
