7666f1e04c5ef30dfdae825fff9c590687304166
afg <afg984@gmail.com>
2015-04-06 01:44:49 +0800

    ability to get syllabus

--- ccxp/fetch.py
+++ ccxp/fetch.py
@@ -4,10 +4,11 @@ import requests
 from lxml.html import fromstring
 
 from ccxp.data import Semester, Department, Course
-from ccxp.decaptcha import decaptcha
 
 
 index_url = 'https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/JH/6/6.2/6.2.9/JH629001.php'  # noqa
+syllabus_url = 'https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/JH/common/Syllabus/1.php'  # noqa
+encoding = 'big5'
 
 
 def xpath1(element, xpath):
@@ -17,16 +18,23 @@ def xpath1(element, xpath):
 
 
 class Browser:
-    def __init__(self):
+    def __init__(self, frozen=None):
         self.session = requests.Session()
         self.index = self.get_index()
+        self.form = self.xpath1('/html/body/div/form')
+        self.ACIXSTORE = xpath1(self.form, 'input[@name="ACIXSTORE"]')
+        self.captcha = self.xpath1('/html/body/div/form/table[2]/tr/td/input')
+        if frozen is not None:
+            self.ACIXSTORE.value = frozen['ACIXSTORE']
+            self.captcha.value = frozen['captcha']
 
     def xpath1(self, xpath):
         return xpath1(self.index, xpath)
 
     def get_index(self):
         response = self.session.get(index_url)
-        return fromstring(response.content, base_url=index_url)
+        response.encoding = encoding
+        return fromstring(response.text, base_url=index_url)
 
     def handle_select(self, type_, xpath):
         select = self.xpath1(xpath)
@@ -59,7 +67,7 @@ class Browser:
         select.value = value
         form = xpath1(self.index, '/html/body/div/form')
         response = self.submit_form(form)
-        response.encoding = 'big5'
+        response.encoding = encoding
         return response.text
 
     def get_courses_by_department(self, value):
@@ -80,10 +88,24 @@ class Browser:
         return urljoin(index_url, img.attrib['src'])
 
     def set_captcha(self, code):
-        captcha = self.xpath1('/html/body/div/form/table[2]/tr/td/input')
-        captcha.value = code
+        self.captcha.value = code
 
     def submit_form(self, form):
         values = form.form_values() + [('cond', 'a')]
         url = form.action
         return self.session.post(url, data=values)
+
+    def get_syllabus(self, course):
+        response = self.session.get(
+            syllabus_url,
+            params=dict(ACIXSTORE=self.ACIXSTORE.value, c_key=course)
+        )
+        response.encoding = encoding
+        document = fromstring(response.text)
+        return document
+
+    def freeze(self):
+        return {
+            'ACIXSTORE': self.ACIXSTORE.value,
+            'captcha': self.captcha.value,
+        }
