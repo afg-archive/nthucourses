a817397d6cf1f61326f8714ad3effbce2bedc999
afg <afg984@gmail.com>
2015-05-29 13:11:02 +0800

    remove social

--- nthucourses/settings.py
+++ nthucourses/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'social.apps.django_app.default',
     'common',
     'courses',
     'logs',
